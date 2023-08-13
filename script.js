8bfe322e6c39888b30c0463a3d9035c4a0f2ddd4
Fernando <fernandoagro.654@gmail.com>
2023-08-13 20:23:58 -0300

    adicionando responsivo

---
+++ script.js
@@ -0,0 +1,9 @@
+let list = document.querySelectorAll('.nav li');
+function active() {
+    list.forEach((i) =>
+    i.classList.remove('active'));
+    this.classList.add('active');
+}
+
+list.forEach((i) => 
+i.addEventListener('click', active));
